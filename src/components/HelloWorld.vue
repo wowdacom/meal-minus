99f80a007af3a906dd94e4f1febe2a114a471c58
jim <wowdacom@gmail.com>
2021-02-17 18:25:46 +0800

    update

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,72 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="hello max-w-screen-sm m-auto">
+    <h1 class="text-center font-bold">Meal Minus</h1>
+    <div class="map-dashboard flex">
+      <div class="map w-3/5">
+        <div id="main-map" class="block relative z-10 w-full h-full"></div>
+      </div>
+      <div class="point-of-foodimages w-2/5 flex flex-wrap">
+        <div
+          :key="item"
+          v-for="item in 4"
+          class="foodimages flex-shrink-0 w-1/2"
+        >
+          {{ item }}
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
+<style>
+@import "~leaflet/dist/leaflet.css";
+@import "~leaflet.markercluster/dist/MarkerCluster.css";
+@import "~leaflet.markercluster/dist/MarkerCluster.Default.css";
+</style>
+
 <script>
+import L from "leaflet";
+import { onMounted } from "vue";
+
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
-    msg: String
-  }
-}
+    msg: String,
+  },
+  setup() {
+    const mapInit = () => {
+      const Layer_ID = "EMAP";
+      const MAP_TYPE = {
+        osm: `https://{s}.tile.osm.org/{z}/{x}/{y}.png`,
+        nlsc: `https://maps.nlsc.gov.tw/S_Maps/wmts?SERVICE=WMTS&REQUEST=GetTile&VERSION=1.0.0&LAYER=${Layer_ID}&STYLE=_null&TILEMATRIXSET=EPSG:3857&TILEMATRIX=EPSG:3857:{z}&TILEROW={y}&TILECOL={x}&FORMAT=image/png`,
+      };
+      const map = L.map("main-map").setView([25.030284, 121.549201], 13);
+
+      L.control.zoom({ position: "bottomright" }).addTo(map);
+
+      L.tileLayer(MAP_TYPE["nlsc"], {
+        maxZoom: 19,
+        //attribution: `&copy; <a target="_blank" rel="noopener noreferrer" href="https://maps.nlsc.gov.tw/">內政部國土測繪中心</a>`
+      }).addTo(map);
+    };
+
+    onMounted(() => {
+      mapInit();
+    });
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
+<style lang="scss" scoped>
+* {
+  -webkit-box-sizing: border-box;
+  -moz-box-sizing: border-box;
+  box-sizing: border-box;
+  border: solid 1px red;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.foodimages {
+  padding-bottom: 50%;
+  height: 0;
 }
 </style>
